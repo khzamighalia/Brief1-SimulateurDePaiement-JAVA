b5744de63c9005044aada05a378ea4d2b2e86756
khzamighalia <elghaliakhzami@gmail.com>
2020-12-06 11:20:02 +0100

    added

---
+++ Brief1/src/brief1.java
@@ -0,0 +1,37 @@
+
+package Brief1;
+import java.util.Scanner;
+
+public class brief1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Bonjour ");
+	    System.out.println("Quel est votre prénom ?");
+	    String prenom = reader.nextLine();
+	    System.out.println("Quel est votre nom ?");
+	    String nom = reader.nextLine();
+	    System.out.println("Bonjour, "+prenom+" "+nom);
+	    System.out.println("Quel est le nombre d'heures travaillés ?");
+	    int heures = reader.nextInt();
+	    if (heures <=60) {
+	    	System.out.println("Quel est le tarif ?");
+		    double tarif = reader.nextDouble();
+		   if(heures<= 40) {
+			   System.out.println("Votre tarif est de"+(tarif*heures));
+		   }
+		   else {
+			  double tt=tarif*40 +(tarif+ 0.5*tarif)*(heures-40);
+			  System.out.println("Votre tarif est de"+tt);
+		   }
+		    
+	    }
+	    else if (heures > 70) {
+	    	System.out.println("informations erronés !");
+	    }
+	    reader.close();
+	
+	}
+
+}
---
+++ Brief1/src/module-info.java
@@ -0,0 +1,2 @@
+module Brief1 {
+}
